30c9e9e95c3883e60fabecf19b2d50afb107d4f8
SEMBARANG-PC <ahmadsulthoni.ext@gmail.com>
2022-04-14 22:05:12 +0700

    lambda fuction

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+#membuat anonimus fuction dengan lambda
+
+kali = lambda argumen : print(argumen)
+kali('test')
+
+#contoh
+
+kali = lambda x,y : x*y
+print(kali(3,4))
+
+#contoh lain
+
+kali = lambda x,y : x*y
+hasil = kali(3,4)
+print(hasil)
