4d606f74d63a642ecc1d199368314942bb1cf7dd
SEMBARANG-PC <ahmadsulthoni.ext@gmail.com>
2022-04-14 21:42:16 +0700

    return value

---
+++ return_value.py
@@ -0,0 +1,27 @@
+#fungsi dengan return value
+
+'''
+def kuadrat(argumen):
+    total = argumen ** 2
+    print('nilai kuadrat dari',argumen,'adalah',total)
+    return total
+a = kuadrat(4)
+print(a)
+'''
+#fungsi dengan return value dan multiple argumen
+
+'''
+def tambah(argumen1,argumen2):
+    total = argumen1 + argumen2
+    print(argumen1,'+',argumen2,'=',total)
+    return total
+a = tambah(4,6)
+print(a)
+'''
+
+def kali(argumen1,argumen2):
+    total = argumen1 * argumen2
+    print(argumen1,'x',argumen2,'=',total)
+    return total
+a = kali(4,6)
+print(a)
