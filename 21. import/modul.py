6d42d8adf1520398f63b15ece185b0cebcfdd07f
SEMBARANG-PC <ahmadsulthoni.ext@gmail.com>
2022-04-16 13:22:38 +0700

    penjelasan import

---
+++ 21. import/modul.py
@@ -0,0 +1,9 @@
+'''
+import gunakanya untuk memasukan/menarik data/fungsi yang sudah ada dari file lain
+contoh = buat file main.py dan modul.py
+'''
+
+data = 'Apa Kabar'
+def cek_modul():
+    print("Hallo gan")
+print('ini adalah modul saya')
