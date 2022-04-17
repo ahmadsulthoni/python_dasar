21c85403a853f82e07e8f2f966085aeffa0d13eb
SEMBARANG-PC <ahmadsulthoni.ext@gmail.com>
2022-04-17 18:54:54 +0700

    membuat module dan package

---
+++ 22. membuat_module/main.py
@@ -0,0 +1,8 @@
+import matematika
+import matematika as m
+
+matematika.tambah(5,6)
+matematika.kurang(9,3)
+m.kurang(8,5)
+m.tambah(8,5)
+
---
+++ 23. membuat package/main.py
@@ -0,0 +1,6 @@
+from sains import tambah
+from sains import kecepatan
+
+tambah(3,4)
+kecepatan(6,5)
+
